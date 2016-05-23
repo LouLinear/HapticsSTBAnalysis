957b445e64dd2894598f376ca87d72e447e036e3
LouLinear <ycloulin@gmail.com>
2016-05-23 16:59:02 -0400

    working straight from csv

---
+++ LouTest/STB_pred.m
@@ -0,0 +1,12 @@
+function ratings = STB_pred(task, rounding)
+    %to be called from pythong to do 
+    addpath '../LIBSVM'
+    addpath '../ML'
+    addpath '../glmnet_matlab'
+    
+    %convertToMat(fileName)
+    [~, csvFileName] = loadcsv('tempcsv', 'evalMat');
+    
+    feats = Signal2Feature('evalMat', csvFileName, task, rounding);
+    ratings = RegressionPredict(feats, rounding);
+end
--- LouTest/STB_pred_python_interface.m
+++
@@ -1,9 +0,0 @@
-function ratings = STB_pred_python_interface( filename, rounding )
-    %to be called from pythong to do 
-    addpath '../LIBSVM'
-    addpath '../ML'
-    
-    feats = Signal2Feature(filename, rounding);
-    ratings = RegressionPredict(feats, rounding);
-end
-
